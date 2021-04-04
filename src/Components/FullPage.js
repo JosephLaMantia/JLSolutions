82b0ad583c765e5234b49060fd7b68fd1023bc54
Joseph LaMantia Jr <40052124+JosephLaMantia@users.noreply.github.com>
2021-04-03 23:54:18 -0400

    Editing project cards to take up the full page.

--- src/Components/FullPage.js
+++ src/Components/FullPage.js
@@ -17,7 +17,7 @@ export default () => {
             </div>
           </section>
         </Slide>
-      <Slide>
+      {/* <Slide>
         <section id='projects'>
           
 
@@ -43,6 +43,48 @@ export default () => {
                   </ul>
               </article>
           </div>
+        </section>
+      </Slide> */}
+
+      <Slide>
+        <section id='projects'>
+                  <div id='project-tile-wrapper'>
+                    <div id='project-description-wrapper'>
+                      <h1>
+                        <a class='project-link' href="https://ttp-art-shop.netlify.app/">ART-SHOP</a>
+                      </h1>
+                      <div id='description-link-wrapper'>
+                        
+                      </div>
+                      <div id='project-img-text-wrapper'>
+                        <div id='link-img-wrapper'>
+                          <div id='repo-link-wrapper'>
+                              <a class='repo-link' href="https://github.com/smilteval/ttp-capstone-art-store-client">Client</a>
+                              <a class='repo-link' href="https://github.com/smilteval/ttp-capstone-art-store-server">Server</a>
+                          </div>
+                          <img class='project-img' src="https://i.imgur.com/BfJQ7DI.png" alt="ART-SHOP"/>
+                        </div>
+                        <div id='project-tech-wrapper'>
+                        <div id='description'>
+                          <p>An online platform where artists and art lovers can post, view, and buy art.</p>
+                          <p class='collaboration'>In collaboration with Smilte Valasinaite, Vincenzo Mezzio, and Sabahet Alovic.
+                          </p>
+                        </div>
+                        <div id='tech-wrapper'>
+                          <ul> <p>Highlighted tech and learning experiences:</p>
+                              <li>Stack: MongoDB, React, Node, Strapi</li>
+                              <li>Material-UI: A react UI framework.</li>
+                              <li>MongoDB: Database to store posts, and user info.</li>
+                              <li>Strapi: A headless content management system.</li>
+                              <li>Cloudinary: An upload hosting service for storing/rendering images quickly.</li>
+                              <li>Netlify: Deployment of front-end.</li>
+                              <li>Heroku: Deployment of back-end.</li>
+                          </ul>
+                        </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
         </section>
       </Slide>
 
